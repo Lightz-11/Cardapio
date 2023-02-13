951920935239224e2ed930fb5cd2c1cdec98b48c
Lightz-11 <lucasanes007@outlook.com>
2023-02-12 21:07:53 -0300

    a

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,8 +2,8 @@ import { styled } from "../../stitches.config";
 
 export const Container = styled("div", {
     
-    width: "100vw",
-    height: "100vh",
+    width: "100%",
+    height: "100%",
     borderRadius: 0,
     border: "none",
     position: 'fixed',
